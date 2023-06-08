cbcb0e57fe82d37475d421ab99856cd437cb2bd2
SugitaMotoki <ce235019@meiji.ac.jp>
2023-06-09 00:59:22 +0900

    仮テストの更新

--- src/test_vm.ts
+++ src/test_vm.ts
@@ -1,27 +1,49 @@
-"use strict";
 /**
  * ブラウザを使わずにVMを検証する
  */
 
-import { vm01, vm02 } from "./vm";
-
-const postfixCalculator = new vm01.PostfixCalculator();
-console.log(postfixCalculator.execute("1 2 + 3 * 4 -"));
+import {
+  // vm01,
+  vm02,
+} from "./vm";
 
 const jump = new vm02.Jump();
+/*
+ * console.log(
+ *   jump.execute(`\
+ * push 10
+ * jump foo
+ * push 20
+ * add
+ * set_global x
+ * push 30
+ * push 40
+ * add
+ * set_global y
+ * get_global x
+ * get_global y
+ * add
+ * label foo
+ * print\
+ * `),
+ * );
+ */
 console.log(
   jump.execute(`\
+push 0
+set_global i
+label CHECK
+get_global i
 push 10
-push 20
+equal
+jump_if END
+get_global i
+print
+get_global i
+push 1
 add
-set_global x
-push 30
-push 40
-add
-set_global y
-get_global x
-get_global y
-add
-print\
+set_global i
+jump CHECK
+label END\
 `),
 );
