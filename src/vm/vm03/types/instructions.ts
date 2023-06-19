4de20c5325de4f5eb814deadcc7e3ddfc80c89f0
SugitaMotoki <ce235019@meiji.ac.jp>
2023-06-19 15:13:04 +0900

    命令セットの一覧をオブジェクトリテラルとして定義

---
+++ src/vm/vm03/types/instructions.ts
@@ -0,0 +1,20 @@
+/**
+ * VM03の命令一覧
+ */
+export const Instruction = {
+  debugShowStack: "debug_show_stack",
+  push: "push",
+  pop: "pop",
+  add: "add",
+  sub: "sub",
+  mul: "mul",
+  div: "div",
+  mod: "mod",
+  equal: "equal",
+  setGlobal: "set_global",
+  getGlobal: "get_global",
+  jump: "jump",
+  jumpIf: "jump_if",
+  jumpIfZero: "jump_if_zero",
+  print: "print",
+};
