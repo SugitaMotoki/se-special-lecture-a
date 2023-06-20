9e9187f97a3a98c90d0126923eaa80bb2ce6a016
SugitaMotoki <ce235019@meiji.ac.jp>
2023-06-21 02:57:55 +0900

    配列の実装

--- src/vm/vm03/array_vm.ts
+++ src/vm/vm03/array_vm.ts
@@ -1,6 +1,90 @@
 import { VirtualMachine03, Instruction } from "./abstract_vm";
 
 export class ArrayVM extends VirtualMachine03 {
+  /** メモリ */
+  private memory: number[][] = [];
+
+  /** 配列名とメモリのインデックスの対応表 */
+  private arrayMap = new Map<string, number>();
+
+  /** 配列を定義する */
+  private _defineArray = (name: string | undefined) => {
+    if (!name) {
+      throw new Error("set_array requires an argument");
+    }
+
+    /** 定義する配列の長さ */
+    const length = this._pop();
+
+    // 長さの文だけ0で埋め、メモリサイズを取得する
+    const memorySize = this.memory.push(new Array(length).fill(0));
+
+    // 配列名とメモリのインデックスを対応付ける
+    this.arrayMap.set(name, memorySize - 1);
+  };
+
+  /** 配列に値を保存する */
+  private _setArray = (name: string | undefined) => {
+    if (!name) {
+      throw new Error("set_array requires an argument");
+    }
+
+    /** 配列内の保存するインデックス */
+    const index = this._pop();
+
+    /** 保存する値 */
+    const value = this._pop();
+
+    /** 保存する配列の場所 */
+    const memoryIndex = this.arrayMap.get(name);
+    if (typeof memoryIndex === "undefined") {
+      throw new Error(`Undefined array name: ${name}`);
+    }
+
+    /** 保存する配列 */
+    const array = this.memory[memoryIndex];
+    if (!array) {
+      throw new Error(`Undefined array name: ${name}`);
+    }
+
+    // 配列に値を保存する
+    array[index] = value;
+
+    // 保存した配列をメモリに戻す
+    this.memory[memoryIndex] = array;
+  };
+
+  /** 配列の要素を取得する */
+  private _getArray = (name: string | undefined) => {
+    if (!name) {
+      throw new Error("get_array requires an argument");
+    }
+
+    /** 配列内の取得するインデックス */
+    const index = this._pop();
+
+    /** 取得する配列の場所 */
+    const memoryIndex = this.arrayMap.get(name);
+    if (typeof memoryIndex === "undefined") {
+      throw new Error(`Undefined array name: ${name}`);
+    }
+
+    /** 取得する配列 */
+    const array = this.memory[memoryIndex];
+    if (!array) {
+      throw new Error(`Undefined array name: ${name}`);
+    }
+
+    /** 取得した値 */
+    const value = array[index];
+    if (typeof value === "undefined") {
+      throw new Error(`Undefined array index: ${index}`);
+    }
+
+    // 取得した値をスタックに積む
+    this.stack.push(value);
+  };
+
   /* eslint max-lines-per-function: "off" */
   /* eslint no-continue: "off" */
   public override execute(input: string): string {
@@ -58,6 +142,15 @@ export class ArrayVM extends VirtualMachine03 {
           break;
         case Instruction.getGlobal:
           this._getGlobal(instruction[1]);
+          break;
+        case Instruction.defineArray:
+          this._defineArray(instruction[1]);
+          break;
+        case Instruction.setArray:
+          this._setArray(instruction[1]);
+          break;
+        case Instruction.getArray:
+          this._getArray(instruction[1]);
           break;
         case Instruction.jump:
           this._jump(instruction[1]);
