36051f9cd32e19d13d9171ee8831488680aa45f7
SugitaMotoki <ce235019@meiji.ac.jp>
2023-06-24 07:27:47 +0900

    サンプルを追加

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -6,7 +6,8 @@ import {
  * import { source01 } from "./ex01";
  * import { source02 } from "./ex02";
  */
-import { source03 } from "./ex03";
+// import { source03 } from "./ex03";
+import { source04 } from "./ex04";
 
 /*
  * const jumpVm = new vm02.Jump();
@@ -18,5 +19,6 @@ import { source03 } from "./ex03";
  * console.log(forVm.execute(source02));
  */
 
-const arrayVm = new vm03.ArrayVM();
-console.log(arrayVm.execute(source03));
+const arrayVm = new vm03.CVMProtoType();
+// console.log(arrayVm.execute(source03));
+console.log(arrayVm.execute(source04));
