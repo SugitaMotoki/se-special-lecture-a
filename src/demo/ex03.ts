c707d71153332976be9776c9a23a8e94a5afd401
SugitaMotoki <ce235019@meiji.ac.jp>
2023-06-24 16:09:09 +0900

    テストを修正

--- src/demo/ex03.ts
+++ src/demo/ex03.ts
@@ -1,5 +1,5 @@
 export const source03 = `
-START:
+MAIN:
         push 0
         set_global i
         push 10
@@ -45,19 +45,19 @@ END:
 `;
 
 /*
-
-#include <stdio.h>
-int main(void)
-{
-    int i;
-    int a[10];
-    for (i = 0; i < 10; i++) {
-      a[i] = i * i;
-    }
-    for (i = 0; i < 10; i++) {
-      printf("%d\n", a[9 - i]);
-    }
-    return 0;
-}
-
-*/
+ *
+ *#include <stdio.h>
+ *int main(void)
+ *{
+ *    int i;
+ *    int a[10];
+ *    for (i = 0; i < 10; i++) {
+ *      a[i] = i * i;
+ *    }
+ *    for (i = 0; i < 10; i++) {
+ *      printf("%d\n", a[9 - i]);
+ *    }
+ *    return 0;
+ *}
+ *
+ */
--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,24 +1,27 @@
-import {
-  // vm02,
-  vm03,
-} from "../vm";
-/*
- * import { source01 } from "./ex01";
- * import { source02 } from "./ex02";
- */
-// import { source03 } from "./ex03";
+import { vm02, vm03 } from "../vm";
+
+import { source00 } from "./ex00";
+import { source01 } from "./ex01";
+import { source02 } from "./ex02";
+import { source03 } from "./ex03";
 import { source04 } from "./ex04";
+import { source05 } from "./ex05";
+import { source06 } from "./ex06";
 
-/*
- * const jumpVm = new vm02.Jump();
- * console.log(jumpVm.execute(source01));
- */
+const jumpVm = new vm02.Jump();
+const forVm = new vm02.For();
+const cVmProto = new vm03.CVMProtoType();
 
-/*
- * const forVm = new vm02.For();
- * console.log(forVm.execute(source02));
- */
+const samples = [
+  { source: source00, vm: jumpVm },
+  { source: source01, vm: forVm },
+  { source: source02, vm: cVmProto },
+  { source: source03, vm: cVmProto },
+  { source: source04, vm: cVmProto },
+  { source: source05, vm: cVmProto },
+  { source: source06, vm: cVmProto },
+];
 
-const arrayVm = new vm03.CVMProtoType();
-// console.log(arrayVm.execute(source03));
-console.log(arrayVm.execute(source04));
+const num = 4;
+const sample = samples[num];
+console.log(sample!.vm.execute(sample!.source));
