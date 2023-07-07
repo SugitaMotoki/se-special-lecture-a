b130f573c327671de8f36fae2c2c47b57e262288
SugitaMotoki <ce235019@meiji.ac.jp>
2023-07-07 15:51:21 +0900

    エラー処理を削除したVMを追加

--- src/vm/vm04/index.ts
+++ src/vm/vm04/index.ts
@@ -2,3 +2,4 @@ export * from "./vm03_copy";
 export * from "./rich_instruction_set_vm";
 export * from "./instruction_order_vm";
 export * from "./short_instruction_vm";
+export * from "./rm_eh_vm";
